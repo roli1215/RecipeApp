e864f9f0177a828f79fc880b9d03d0302a4869e1
Karczub Roland <karczubroland@gmail.com>
2024-07-18 12:50:12 +0200

    swagger works

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,10 +8,8 @@ const app = express();
 const port = process.env.PORT ;
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerOptions = require('./swagger/configuration').options;
+const swaggerDocument = require('./swagger-output.json');
 
-const specs = swaggerJsdoc(swaggerOptions);
 
 app.use(express.json());
 app.use(function (req, res, next) {
@@ -20,7 +18,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.get('/', (req, res) => {
---
+++ backend/src/swagger.ts
@@ -0,0 +1,32 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'Recipe App API',
+        version: '1.0.0',
+    },
+    servers: [
+        { url: 'http://localhost:3000' },
+    ],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+            },
+        },
+    },
+    security: [
+        {
+            bearerAuth: [],
+        },
+    ],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./routes/index.ts'];   
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    console.log('Swagger documentation generated successfully.');
+});
